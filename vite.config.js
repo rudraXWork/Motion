f7feaff7d1d636c8f28adeb8646f07ee7a610d35
Rudra Jena <rnj.rudra3@gmail.com>
2026-01-21 12:44:17 +0530

    Fix Vite base path after repo rename

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/scrollview-app/'   // <-- add this outside the plugins array
+  base: '/Motion/'
 })
